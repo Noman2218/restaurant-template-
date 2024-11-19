9942c6fa328eee29bbad6b588bc135929608c354
M.Noman <153206686+Noman2218@users.noreply.github.com>
2024-11-19 18:40:47 +0500

    header updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
 
-
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
+     <Header/>
     </>
   )
 }
